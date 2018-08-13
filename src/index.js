a24ce05e1e659be3529316785277db31699622a8
3went2 <webb.png@gmail.com>
2018-08-13 16:38:41 +0800

    completely redesign

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
+import WebSocket from 'ws';
+import pify from 'pify';
 
-export Server from './Server';
-export Client from './Client';
+function extendWS(ws) {
+	if (ws._promised) return ws;
+
+	function promisify(method) {
+		const callbackMethod = `${method}Callback`;
+		ws[callbackMethod] = ws[method];
+		ws[method] = pify(ws[callbackMethod]);
+	}
+
+	ws._promised = true;
+	promisify('send');
+	promisify('ping');
+	promisify('pong');
+	return ws;
+}
+
+class PromiseWebSocketServer extends WebSocket.Server {
+	static async create(options) {
+		return new Promise((resolve, reject) => {
+			const wss = new PromiseWebSocketServer(options);
+			const { server, noServer } = options;
+			const done = () => resolve(wss);
+			if (noServer || (server && server.listening)) process.nextTick(done);
+			else wss.once('listening', done);
+			wss.once('error', reject);
+		});
+	}
+
+	on(event, listener) {
+		if (event === 'connection') {
+			return super.on(event, (ws) => listener(extendWS(ws)));
+		}
+		return super.on(event, listener);
+	}
+
+	close() {
+		return pify(super.close.bind(this))();
+	}
+}
+
+export default class PromiseWebSocket extends WebSocket {
+	static Server = PromiseWebSocketServer;
+	static WebSocket = WebSocket;
+
+	static create(...args) {
+		return new Promise((resolve, reject) => {
+			const ws = new PromiseWebSocket(...args);
+			ws.once('open', () => resolve(ws));
+			ws.once('error', reject);
+		});
+	}
+
+	constructor(...args) {
+		super(...args);
+		extendWS(this);
+	}
+}
--- test/test.js
+++ test/test.js
@@ -1,460 +1,123 @@
-import { Server, Client } from '../src';
+import WebSocket from '../src';
 import delay from 'delay';
 import net from 'net';
-import WebSocket from 'ws';
 import pify from 'pify';
 
-let _server;
-let _clients = [];
-
-const createServer = async (...args) => {
-	_server = await Server.create(...args);
-	return _server;
-};
-
-const createClient = async (...args) => {
-	const client = await Client.create(...args);
-	_clients.push(client);
-	return client;
-};
-
-const connectClient = async (url, waitUntil) => {
-	return Client.connect(url, async (client) => {
-		_clients.push(client);
-		return waitUntil(client);
-	});
-};
-
-const autoReconnectClient = async (url, waitUntil) => {
-	return Client.autoReconnect(url, async (client) => {
-		_clients.push(client);
-		return waitUntil(client);
-	});
-};
+const servers = [];
+const clients = [];
 
 afterEach(async () => {
-	await Promise.all(_clients.map(async (client) => client.close()));
-	await _server.close().catch(() => {});
-	_clients = [];
+	await Promise.all(
+		[
+			...clients.map(async (client) => client.terminate()),
+			...servers.map(async (server) => server.close()),
+		].filter(Boolean),
+	);
+	servers.length = 0;
+	clients.length = 0;
 });
 
-describe('Client.create()', () => {
-	test('create with address', async () => {
+describe('promise-ws', () => {
+	test('create', async () => {
 		const port = 3000;
-		await createServer({ port });
-		const client = await createClient(`ws://127.0.0.1:${port}`);
-		expect(client.ws().readyState).toBe(1);
+		const wss = await WebSocket.Server.create({ port });
+		const ws = await WebSocket.create(`ws://127.0.0.1:${port}`);
+		servers.push(wss);
+		clients.push(ws);
+		expect(ws).toBeInstanceOf(WebSocket.WebSocket);
+		expect(wss).toBeInstanceOf(WebSocket.Server);
 	});
 
-	test('create with options object', async () => {
+	test('create with net server', async () => {
+		const server = new net.Server();
+		await pify(::server.listen)();
+		const wss = await WebSocket.Server.create({ server });
+		servers.push(server, wss);
+		expect(wss).toBeInstanceOf(WebSocket.Server);
+	});
+
+	test('throw error when missing args', async () => {
+		await expect(WebSocket.Server.create()).rejects.toBeDefined();
+	});
+
+	test('ws.send', async () => {
 		const port = 3000;
-		await createServer({ port });
-		const client = await createClient({
-			address: `ws://127.0.0.1:${port}`,
+		const listener = jest.fn();
+		const wss = await WebSocket.Server.create({ port });
+		wss.on('connection', (ws) => {
+			ws.on('message', listener);
 		});
-		expect(client.ws().readyState).toBe(1);
+		const ws = await WebSocket.create(`ws://127.0.0.1:${port}`);
+		servers.push(wss);
+		clients.push(ws);
+		await ws.send('foo');
+		await delay(100);
+		expect(listener).toHaveBeenCalledTimes(1);
+		expect(listener).toHaveBeenLastCalledWith('foo');
 	});
-});
 
-describe('Client.connect()', () => {
-	test('connect', async () => {
+	test('throw error if ws disconnected before ws.send', async () => {
 		const port = 3000;
-		const server = await createServer({ port });
-		const url = `ws://127.0.0.1:${port}`;
-		const handleSay = jest.fn(async () => 'world');
-		const res = await connectClient(url, async (client) => {
-			server.onReply('say', handleSay);
-			return client.request('say', 'hello');
+		const listener = jest.fn();
+		const wss = await WebSocket.Server.create({ port });
+		wss.on('connection', (ws) => {
+			ws.on('message', listener);
 		});
-		expect(res).toBe('world');
-		expect(handleSay).toHaveBeenCalledWith('hello');
+		const ws = await WebSocket.create(`ws://127.0.0.1:${port}`);
+		servers.push(wss);
+		clients.push(ws);
+		ws.terminate();
+		await expect(ws.send('foo')).rejects.toBeDefined();
 	});
 
-	test('should throw error if error occurs', async () => {
+	test('wss.send', async () => {
 		const port = 3000;
-		await createServer({ port });
-		await expect(
-			connectClient(`ws://127.0.0.1:${port}`, async () => {
-				throw new Error('err');
-			}),
-		).rejects.toBeDefined();
-	});
-
-	test('should throw error if failed to connect to server', async () => {
-		await expect(connectClient('ws://127.0.0.1')).rejects.toBeDefined();
-	});
-
-	test('should throw error if server closed', async () => {
-		const port = 3000;
-		const server = await createServer({ port });
-		await expect(
-			connectClient(`ws://127.0.0.1:${port}`, async () => {
-				await server.close();
-				await delay(100);
-			}),
-		).rejects.toBeDefined();
-	});
-});
-
-describe('Client.autoReconnect()', () => {
-	test('should connect if success to connect', async () => {
-		const port = 3000;
-		const server = await createServer({ port });
-		const url = `ws://127.0.0.1:${port}`;
-		const handleSay = jest.fn(async () => 'world');
-		const res = await autoReconnectClient(url, async (client) => {
-			server.onReply('say', handleSay);
-			return client.request('say', 'hello');
+		const listener = jest.fn();
+		const wss = await WebSocket.Server.create({ port });
+		wss.on('connection', async (ws) => {
+			await delay(100);
+			await ws.send('foo');
 		});
-		expect(res).toBe('world');
-		expect(handleSay).toHaveBeenCalledWith('hello');
+		const ws = await WebSocket.create(`ws://127.0.0.1:${port}`);
+		servers.push(wss);
+		clients.push(ws);
+		ws.on('message', listener);
+		await delay(200);
+		expect(listener).toHaveBeenCalledTimes(1);
+		expect(listener).toHaveBeenLastCalledWith('foo');
 	});
 
-	test('should reconnect if failed to connect', async () => {
+	test('throw error if ws disconnected before ws.send', async () => {
 		const port = 3000;
-		let server;
-		const url = `ws://127.0.0.1:${port}`;
-		const handleSay = jest.fn(async () => 'world');
-		setTimeout(async () => (server = await createServer({ port })), 15);
-		const res = await autoReconnectClient(
-			url,
-			async (client) => {
-				server.onReply('say', handleSay);
-				return client.request('say', 'hello');
-			},
-			10,
-		);
-		expect(res).toBe('world');
-		expect(handleSay).toHaveBeenCalledWith('hello');
-	});
-
-	test('should throw error if error occurs', async () => {
-		const port = 3000;
-		await createServer({ port });
-		const url = `ws://127.0.0.1:${port}`;
-		await expect(
-			autoReconnectClient(url, async () => {
-				throw new Error();
-			}),
-		).rejects.toBeDefined();
-	});
-});
-
-describe('client', () => {
-	test('client.request()', async () => {
-		const port = 3000;
-		await createServer({ port });
-		const client = await createClient(`ws://127.0.0.1:${port}`);
-		await client.request('hello');
-	});
-
-	test('client.request() and wait for response', async () => {
-		const port = 3000;
-		const server = await createServer({ port });
-		server.onReply('say', async (data) => {
-			expect(data).toBe('hello');
-			await delay(10);
-			return 'world';
+		const called = jest.fn();
+		const wss = await WebSocket.Server.create({ port });
+		wss.on('connection', async (ws) => {
+			await wss.close();
+			await expect(ws.send('foo')).rejects.toBeDefined();
+			called();
 		});
-		const client = await createClient(`ws://127.0.0.1:${port}`);
-		const res = await client.request('say', 'hello');
-		expect(res).toBe('world');
+		const ws = await WebSocket.create(`ws://127.0.0.1:${port}`);
+		servers.push(wss);
+		clients.push(ws);
+		await delay(200);
+		expect(called).toHaveBeenCalledTimes(1);
 	});
 
-	test('client.waitFor()', async () => {
+	test('wss on connection in multiple times', async () => {
 		const port = 3000;
-		const server = await createServer({ port });
-		const client = await createClient(`ws://127.0.0.1:${port}`);
-		setTimeout(() => {
-			server.request('hello', 'world');
-		}, 10);
-		const [res] = await client.waitFor('hello');
-		expect(res).toBe('world');
-	});
-
-	test('client.onReply()', async () => {
-		const port = 3000;
-		const server = await createServer({ port });
-		const client = await createClient(`ws://127.0.0.1:${port}`);
-		const handleSay = jest.fn(async (data) => {
-			expect(data).toBe('hello');
-			await delay(10);
+		const listener = jest.fn();
+		const wss = await WebSocket.Server.create({ port });
+		wss.on('connection', (ws) => {
+			ws.on('message', listener);
 		});
-		client.onReply('say', handleSay);
-		await server.request('say', 'hello');
-		expect(handleSay).toHaveBeenCalledTimes(1);
-	});
-
-	test('client.reply()', async () => {
-		const port = 3000;
-		const server = await createServer({ port });
-		const client = await createClient(`ws://127.0.0.1:${port}`);
-		const handleSay = jest.fn(async (data) => {
-			expect(data).toBe('hello');
-			await delay(10);
+		wss.on('connection', (ws) => {
+			ws.on('message', listener);
 		});
-		client.reply('say', handleSay);
-		await server.request('say', 'hello');
-		expect(handleSay).toHaveBeenCalledTimes(1);
-	});
-
-	test('client.addReply()', async () => {
-		const port = 3000;
-		const server = await createServer({ port });
-		const client = await createClient(`ws://127.0.0.1:${port}`);
-		const handleSay = jest.fn(async (data) => {
-			expect(data).toBe('hello');
-			await delay(10);
-		});
-		client.addReply('say', handleSay);
-		await server.request('say', 'hello');
-		expect(handleSay).toHaveBeenCalledTimes(1);
-	});
-
-	test('client.removeReply()', async () => {
-		const port = 3000;
-		const server = await createServer({ port });
-		const client = await createClient(`ws://127.0.0.1:${port}`);
-		const handleSay = jest.fn();
-		client.addReply('say', handleSay);
-		client.removeReply('say', handleSay);
-		await server.request('say', 'hello');
-		expect(handleSay).toHaveBeenCalledTimes(0);
-	});
-
-	test('client.replyOnce()', async () => {
-		const port = 3000;
-		const server = await createServer({ port });
-		const client = await createClient(`ws://127.0.0.1:${port}`);
-		const handleSay = jest.fn();
-		client.replyOnce('say', handleSay);
-		await server.request('say', 'yes');
-		await server.request('say', 'no');
-		expect(handleSay).toHaveBeenCalledWith('yes');
-		expect(handleSay).toHaveBeenCalledTimes(1);
-	});
-
-	test('client.replyOnce() and client.removeReply()', async () => {
-		const port = 3000;
-		const server = await createServer({ port });
-		const client = await createClient(`ws://127.0.0.1:${port}`);
-		const handleSay = jest.fn();
-		client.replyOnce('say', handleSay);
-		client.removeReply('say', handleSay);
-		await server.request('say', 'no');
-		expect(handleSay).toHaveBeenCalledTimes(0);
-	});
-
-	test('client.replyCount()', async () => {
-		const port = 3000;
-		await createServer({ port });
-		const client = await createClient(`ws://127.0.0.1:${port}`);
-		const reply1 = () => {};
-		const reply2 = () => {};
-
-		expect(client.replyCount('say')).toBe(0);
-		client.addReply('say', reply1);
-		expect(client.replyCount('say')).toBe(1);
-		client.addReply('say', reply2);
-		expect(client.replyCount('say')).toBe(2);
-		client.removeReply('say', reply1);
-		expect(client.replyCount('say')).toBe(1);
-		client.removeReply('say', reply2);
-		expect(client.replyCount('say')).toBe(0);
-	});
-
-	test('client.ws()', async () => {
-		const port = 3000;
-		await createServer({ port });
-		const client = await createClient(`ws://127.0.0.1:${port}`);
-		expect(client.ws()).toBeInstanceOf(WebSocket);
-	});
-
-	test('client.onReply() with errorHandler', async (done) => {
-		const port = 3000;
-		const server = await createServer({ port });
-		const errorHandler = jest.fn(done);
-		server.on('connection', (ws) => {
-			const client = server.clients.get(ws);
-			client.onReply('say', async () => server.close(), errorHandler);
-		});
-		const client = await createClient(`ws://127.0.0.1:${port}`);
-		client.request('say');
-	});
-
-	test('client.requestClose()', async () => {
-		const port = 3000;
-		const server = await createServer({ port });
-		const client = await createClient(`ws://127.0.0.1:${port}`);
-		const wss = server.wss();
-		server.onReplyClose(() => true);
-		expect(!!wss._server).toBe(true);
-		await client.requestClose();
-		expect(!!wss._server).toBe(false);
-	});
-
-	test('server reject client.requestClose()', async () => {
-		const port = 3000;
-		const server = await createServer({ port });
-		const client = await createClient(`ws://127.0.0.1:${port}`);
-		const wss = server.wss();
-		server.onReplyClose(() => false);
-		await expect(client.requestClose()).rejects.toBeDefined();
-		expect(!!wss._server).toBe(true);
-	});
-});
-
-describe('Server.create()', () => {
-	let netServer;
-
-	afterEach((done) => {
-		if (netServer) {
-			netServer.close(done);
-		}
-		else {
-			done();
-		}
-	});
-
-	test('should throw error if missing argument', async () => {
-		await expect(createServer()).rejects.toBeDefined();
-	});
-
-	test('create with `port`', async () => {
-		const port = 3000;
-		const server = await createServer({ port });
-		await expect(server).toBeInstanceOf(Server);
-	});
-
-	test('create with `noServer`', async () => {
-		const server = await createServer({ noServer: true });
-		await expect(server).toBeInstanceOf(Server);
-	});
-
-	test('create with `server`', async () => {
-		netServer = new net.Server();
-		await pify(::netServer.listen)();
-		const server = await createServer({ server: netServer });
-		await expect(server).toBeInstanceOf(Server);
-	});
-
-	test('should throw error if error occurs', async () => {
-		const port = 3000;
-		netServer = new net.Server();
-		await pify(::netServer.listen)(port);
-		await expect(createServer({ port })).rejects.toBeDefined();
-	});
-});
-
-describe('server', () => {
-	test('server.request()', async () => {
-		const port = 3000;
-		const server = await createServer({ port });
-		const client1 = await createClient(`ws://127.0.0.1:${port}`);
-		const client2 = await createClient(`ws://127.0.0.1:${port}`);
-		const handleClientSay1 = jest.fn(async () => 1);
-		const handleClientSay2 = jest.fn(async () => 2);
-		client1.onReply('say', handleClientSay1);
-		client2.onReply('say', handleClientSay2);
-		const res = await server.request('say', 'hello');
-		expect(handleClientSay1).toHaveBeenCalledTimes(1);
-		expect(handleClientSay1).toHaveBeenCalledWith('hello');
-		expect(handleClientSay2).toHaveBeenCalledTimes(1);
-		expect(handleClientSay2).toHaveBeenCalledWith('hello');
-		expect(res).toEqual(expect.arrayContaining([1, 2]));
-	});
-
-	test('server.onReply()', async () => {
-		const port = 3000;
-		const server = await createServer({ port });
-		const client = await createClient(`ws://127.0.0.1:${port}`);
-		const handleSay = jest.fn(async () => {
-			await delay(10);
-		});
-		server.onReply('say', handleSay);
-		await client.request('say', 'hello');
-		expect(handleSay).toHaveBeenCalledTimes(1);
-		expect(handleSay).toHaveBeenCalledWith('hello');
-	});
-
-	test('server.reply()', async () => {
-		const port = 3000;
-		const server = await createServer({ port });
-		const client = await createClient(`ws://127.0.0.1:${port}`);
-		const handleSay = jest.fn(async () => {
-			await delay(10);
-		});
-		server.reply('say', handleSay);
-		await client.request('say', 'hello');
-		expect(handleSay).toHaveBeenCalledTimes(1);
-		expect(handleSay).toHaveBeenCalledWith('hello');
-	});
-
-	test('server.addReply()', async () => {
-		const port = 3000;
-		const server = await createServer({ port });
-		const client = await createClient(`ws://127.0.0.1:${port}`);
-		const handleSay = jest.fn(async () => {
-			await delay(10);
-		});
-		server.addReply('say', handleSay);
-		await client.request('say', 'hello');
-		expect(handleSay).toHaveBeenCalledTimes(1);
-		expect(handleSay).toHaveBeenCalledWith('hello');
-	});
-
-	test('server.removeReply()', async () => {
-		const port = 3000;
-		const server = await createServer({ port });
-		const client = await createClient(`ws://127.0.0.1:${port}`);
-		const handleSay = jest.fn();
-		server.addReply('say', handleSay);
-		server.removeReply('say', handleSay);
-		await client.request('say', 'hello');
-		expect(handleSay).toHaveBeenCalledTimes(0);
-	});
-
-	test('server.replyCount()', async () => {
-		const port = 3000;
-		const server = await createServer({ port });
-		const reply1 = () => {};
-		const reply2 = () => {};
-
-		expect(server.replyCount('say')).toBe(0);
-		server.addReply('say', reply1);
-		expect(server.replyCount('say')).toBe(1);
-		server.addReply('say', reply2);
-		expect(server.replyCount('say')).toBe(2);
-		server.removeReply('say', reply1);
-		expect(server.replyCount('say')).toBe(1);
-		server.removeReply('say', reply2);
-		expect(server.replyCount('say')).toBe(0);
-	});
-
-	test('server.wss()', async () => {
-		const port = 3000;
-		const server = await createServer({ port });
-		expect(server.wss()).toBeInstanceOf(WebSocket.Server);
-	});
-
-	test('server.clients', async () => {
-		const port = 3000;
-		const server = await createServer({ port });
-		await createClient(`ws://127.0.0.1:${port}`);
-		await createClient(`ws://127.0.0.1:${port}`);
-		expect(server.clients.size).toBe(2);
-		server.clients.forEach((client) => expect(client).toBeInstanceOf(Client));
-	});
-
-	test('server.onConnection()', async () => {
-		const port = 3000;
-		const server = await createServer({ port });
-		const handler = jest.fn((client) => {
-			expect(client).toBeInstanceOf(Client);
-		});
-		server.onConnection(handler);
-		await createClient(`ws://127.0.0.1:${port}`);
-		await createClient(`ws://127.0.0.1:${port}`);
-		expect(handler).toHaveBeenCalledTimes(2);
+		const ws = await WebSocket.create(`ws://127.0.0.1:${port}`);
+		servers.push(wss);
+		clients.push(ws);
+		await ws.send('foo');
+		await delay(100);
+		expect(listener).toHaveBeenCalledTimes(2);
 	});
 });
